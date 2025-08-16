e204f30d735ba3549eae020c606368a2459d12d8
SINTHIA SOKH <116630488+sinthia88@users.noreply.github.com>
2025-08-16 06:59:17 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Send index.html for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
